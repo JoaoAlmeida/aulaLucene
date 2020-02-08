f8ae8f66eb66d6ab2ee4f1de7c0670f00042be23
João Paulo D. Almeida <jpalmeida.uefs@gmail.com>
2020-02-08 14:07:30 -0300

    Create Searcher
    Create the search class example

--- src/main/java/aulaLucene/Index.java
+++ src/main/java/aulaLucene/Index.java
@@ -1,7 +1,6 @@
 package aulaLucene;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
@@ -10,23 +9,31 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class Index {
 
+	private static final String INDEX_DIR = "D:\\Documents\\GitHub\\aulaLucene\\index";
 	
-	public static IndexWriter createWriter() throws IOException, URISyntaxException 
+	public IndexWriter createWriter() throws IOException, URISyntaxException 
 	{
-	    FSDirectory dir = FSDirectory.open(Paths.get(new URI("D:\\Documents\\GitHub\\aulaLucene\\index")));
+	    FSDirectory dir = FSDirectory.open((Paths.get(INDEX_DIR)));
 	    IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
 	    IndexWriter writer = new IndexWriter(dir, config);
 	    
 	    return writer;
 	}
 	
-	public static Document createDocument(Integer id, String firstName, String lastName, String website) 
+	public Document createDocument(Integer id, String firstName, String lastName, String website) 
 	{
 	    Document document = new Document();
 	    document.add(new StringField("id", id.toString() , Store.YES));
@@ -35,4 +42,27 @@ public class Index {
 	    document.add(new TextField("website", website , Store.YES));
 	    return document;
 	}
+	
+	public TopDocs searchByFirstName(String firstName, IndexSearcher searcher) throws Exception
+	    {
+	        QueryParser qp = new QueryParser("firstName", new StandardAnalyzer());
+	        Query firstNameQuery = qp.parse(firstName);
+	        TopDocs hits = searcher.search(firstNameQuery, 10);
+	        return hits;
+	    }
+	 
+	 public TopDocs searchById(Integer id, IndexSearcher searcher) throws Exception
+	    {
+	        QueryParser qp = new QueryParser("id", new StandardAnalyzer());
+	        Query idQuery = qp.parse(id.toString());
+	        TopDocs hits = searcher.search(idQuery, 10);
+	        return hits;
+	    }
+	 
+	    public IndexSearcher createSearcher() throws IOException {
+	        Directory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+	        IndexReader reader = DirectoryReader.open(dir);
+	        IndexSearcher searcher = new IndexSearcher(reader);
+	        return searcher;
+	    }
 }
--- src/main/java/aulaLucene/LuceneWriteIndexExample.java
+++ src/main/java/aulaLucene/LuceneWriteIndexExample.java
@@ -1,33 +1,27 @@
 package aulaLucene;
 
 import java.util.List;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 
 public class LuceneWriteIndexExample {
-	
-	private static final String INDEX_DIR = "D:\\Documents\\GitHub\\aulaLucene\\index";
 	 
     public static void main(String[] args) throws Exception 
     {
-    	IndexWriter writer = createWriter();
+    	
+    	Index index = new Index();
+    	
+    	IndexWriter writer = index.createWriter();
         
         List<Document> documents = new ArrayList<Document>();
          
-        Document document1 = createDocument(1, "Lokesh", "Gupta", "howtodoinjava.com");
+        //id, primeiro nome, ultimo nome, website
+        Document document1 = index.createDocument(1, "João Paulo", "Almeida", "howtodoinjava.com");
         documents.add(document1);
          
-        Document document2 = createDocument(2, "Brian", "Schultz", "example.com");
+        Document document2 = index.createDocument(2, "Brian", "Schultz", "example.com");
         documents.add(document2);
          
         //Let's clean everything first
@@ -36,23 +30,8 @@ public class LuceneWriteIndexExample {
         writer.addDocuments(documents);
         writer.commit();
         writer.close();
+        
+        System.out.println("Index Criado");
     }
- 
-    private static Document createDocument(Integer id, String firstName, String lastName, String website) 
-    {
-        Document document = new Document();
-        document.add(new StringField("id", id.toString() , Store.YES));
-        document.add(new TextField("firstName", firstName , Store.YES));
-        document.add(new TextField("lastName", lastName , Store.YES));
-        document.add(new TextField("website", website , Store.YES));
-        return document;
-    }
- 
-    private static IndexWriter createWriter() throws IOException 
-    {
-        FSDirectory dir = FSDirectory.open(Paths.get(INDEX_DIR));
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-        IndexWriter writer = new IndexWriter(dir, config);
-        return writer;
-    }
+
 }
